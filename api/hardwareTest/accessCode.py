8c218ffaba57528fd00197ae0483b84cd08fa425
Daniel Nduati <dannduati2@gmail.com>
2021-02-04 23:28:57 +0300

    updated accesscode.py

--- api/hardwareTest/accessCode.py
+++ api/hardwareTest/accessCode.py
@@ -1,10 +1,13 @@
 import urequests as requests
 import ujson
+import connectWifi
 
+#connect to the wifi
+connectWifi.connect()
 
 url = "https://dev-42331gx2.us.auth0.com/oauth/token"
 headers = {'content-type':'application/json'}
 data = '{"client_id":"Goi7MZiF0YCI4YAykw5gbd5Xky8s4JUj","client_secret":"v6sEKlu8WE6OVGpSXamVLEM8W9_IKSwg6eeaQJmY0qdCIrLfhfQP1j-GRDSOEL6K","audience":"sams-dwh-web-api","grant_type":"client_credentials"}'
-response = urequests.post(url,data=data,headers=headers)
+response = requests.post(url,data=data,headers=headers)
 results = response.json()
 print(results)
