11f793447a87ec8a946ece76ab2691d19d0954f2
Daniel Nduati <dannduati2@gmail.com>
2021-02-04 22:18:40 +0300

    Updated accessCode.py

--- api/hardwareTest/accessCode.py
+++ api/hardwareTest/accessCode.py
@@ -2,4 +2,9 @@ import urequests as requests
 import ujson
 
 
-url = ""
+url = "https://dev-42331gx2.us.auth0.com/oauth/token"
+headers = {'content-type':'application/json'}
+data = '{"client_id":"Goi7MZiF0YCI4YAykw5gbd5Xky8s4JUj","client_secret":"v6sEKlu8WE6OVGpSXamVLEM8W9_IKSwg6eeaQJmY0qdCIrLfhfQP1j-GRDSOEL6K","audience":"sams-dwh-web-api","grant_type":"client_credentials"}'
+response = urequests.post(url,data=data,headers=headers)
+results = response.json()
+print(results)
