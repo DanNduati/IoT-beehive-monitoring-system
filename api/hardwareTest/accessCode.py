f70e8f2408e30935e6d5066fd4ed2c03ef4190fb
Daniel Nduati <dannduati2@gmail.com>
2021-02-16 11:21:32 +0300

    Get access code with the microcontroller

--- api/hardwareTest/accessCode.py
+++ api/hardwareTest/accessCode.py
@@ -7,7 +7,9 @@ connectWifi.connect()
 
 url = "https://dev-42331gx2.us.auth0.com/oauth/token"
 headers = {'content-type':'application/json'}
-data = '{"client_id":"Goi7MZiF0YCI4YAykw5gbd5Xky8s4JUj","client_secret":"v6sEKlu8WE6OVGpSXamVLEM8W9_IKSwg6eeaQJmY0qdCIrLfhfQP1j-GRDSOEL6K","audience":"sams-dwh-web-api","grant_type":"client_credentials"}'
+data ='{"client_id":"EptrWV0x17H0AlOoTvz06akdZ5imV4U5","client_secret":"M5c70lGDh8g9Zp2cItFbQvIJnpGtJqX7tTwq3MC7wqWkR_9OD4j7iilAW0vVgzo9","audience":"sams-dwh-web-api","grant_type":"client_credentials"}'
 response = requests.post(url,data=data,headers=headers)
 results = response.json()
-print(results)
+accessToken = results['access_token']
+bearer = results['Bearer']
+print(accessToken)
